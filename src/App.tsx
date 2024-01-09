9250aad9cbc4ee9a06d3a842f3c8fedd1f930ebe
Apu Sutra Dhar <apusutradhar77@gmail.com>
2024-01-09 22:18:23 +0600

    component based provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Menu, MenuItem } from "./components/Menu";
 import ThemeToggleButton from "./components/ThemeToggleButton";
 
 function App() {
   return (
     <>
       <ThemeToggleButton></ThemeToggleButton>
+      <Menu>
+        <MenuItem></MenuItem>
+      </Menu>
     </>
   );
 }
